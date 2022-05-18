864c88d7c9348e62c854a3fec5953399868bbd3b
arora <aroramadhav65@gmail.com>
2022-05-18 20:11:55 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter basic calculator',
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Calculation(),
       ),
