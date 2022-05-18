b42afdd78b99e9b62bf47bab0498be1b1b00284e
arora <aroramadhav65@gmail.com>
2022-05-18 20:04:33 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -50,28 +50,6 @@ class _HomePageState extends State<HomePage> {
     '=',
     '+',
   ];
-  final List<String> buttons = [
-    'C',
-    '+/-',
-    '%',
-    'DEL',
-    '7',
-    '8',
-    '9',
-    '/',
-    '4',
-    '5',
-    '6',
-    'x',
-    '1',
-    '2',
-    '3',
-    '-',
-    '0',
-    '.',
-    '=',
-    '+',
-  ];
 
   @override
   Widget build(BuildContext context) {
