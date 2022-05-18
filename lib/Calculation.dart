9f7f00f5097041506e51fc5a96debb399345dd75
arora <aroramadhav65@gmail.com>
2022-05-18 20:30:48 +0530

    Update

--- lib/Calculation.dart
+++ lib/Calculation.dart
@@ -15,8 +15,16 @@ class _CalculationState extends State<Calculation> {
       children: [
         ResultDisplay(text: '0'),
         Row(
-          children: [],
-        ),
+  children: [
+    CalculatorButton(
+      label: '7',
+      onTap: () => {},
+      size: 90,
+      backgroundColor: Colors.white,
+      labelColor: Colors.black,
+    )
+  ],
+)
       ],
     );
   }
