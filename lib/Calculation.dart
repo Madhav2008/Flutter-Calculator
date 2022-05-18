9a7b10486a171d2257e341955f0f2bafc78f5bd5
arora <aroramadhav65@gmail.com>
2022-05-18 20:25:41 +0530

    Update

--- lib/Calculation.dart
+++ lib/Calculation.dart
@@ -11,15 +11,13 @@ class _CalculationState extends State<Calculation> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-    children: [
+    return Column(children: [
       ResultDisplay(text: '0'),
       Row(
         children: [
           // Here we want to place the buttons of the first Row
         ],
       )
-    ]
-  );
+    ]);
   }
 }
