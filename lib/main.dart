569854a65c7cd3d89d8eb62623f0bcee7943c3e7
arora <aroramadhav65@gmail.com>
2022-05-18 23:42:57 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -200,7 +200,7 @@ class _SimpleCalculatorState extends State<SimpleCalculator> {
                     ),
                   ],
                 ),
-              )
+              ),
             ],
           ),
         ],
