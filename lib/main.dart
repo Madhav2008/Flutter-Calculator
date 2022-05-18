ab3d7974cef05db50a9ba7f72dbf0eba9e9be4a5
arora <aroramadhav65@gmail.com>
2022-05-18 17:50:21 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,203 @@
 import 'package:flutter/material.dart';
+import 'buttons.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 void main() {
-  runApp(
-    const MyApp(),
-  );
+runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Calculator',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+@override
+Widget build(BuildContext context) {
+	return MaterialApp(
+	debugShowCheckedModeBanner: false,
+	home: HomePage(),
+	); // MaterialApp
+}
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+class HomePage extends StatefulWidget {
+@override
+_HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+var userInput = '';
+var answer = '';
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+// Array of button
+final List<String> buttons = [
+	'C',
+	'+/-',
+	'%',
+	'DEL',
+	'7',
+	'8',
+	'9',
+	'/',
+	'4',
+	'5',
+	'6',
+	'x',
+	'1',
+	'2',
+	'3',
+	'-',
+	'0',
+	'.',
+	'=',
+	'+',
+];
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
+@override
+Widget build(BuildContext context) {
+	return Scaffold(
+	appBar: new AppBar(
+		title: new Text("Calculator"),
+	), //AppBar
+	backgroundColor: Colors.white38,
+	body: Column(
+		children: <Widget>[
+		Expanded(
+			child: Container(
+			child: Column(
+				mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+				children: <Widget>[
+					Container(
+					padding: EdgeInsets.all(20),
+					alignment: Alignment.centerRight,
+					child: Text(
+						userInput,
+						style: TextStyle(fontSize: 18, color: Colors.white),
+					),
+					),
+					Container(
+					padding: EdgeInsets.all(15),
+					alignment: Alignment.centerRight,
+					child: Text(
+						answer,
+						style: TextStyle(
+							fontSize: 30,
+							color: Colors.white,
+							fontWeight: FontWeight.bold),
+					),
+					)
+				]),
+			),
+		),
+		Expanded(
+			flex: 3,
+			child: Container(
+			child: GridView.builder(
+				itemCount: buttons.length,
+				gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+					crossAxisCount: 4),
+				itemBuilder: (BuildContext context, int index) {
+					// Clear Button
+					if (index == 0) {
+					return MyButton(
+						buttontapped: () {
+						setState(() {
+							userInput = '';
+							answer = '0';
+						});
+						},
+						buttonText: buttons[index],
+						color: Colors.blue[50],
+						textColor: Colors.black,
+					);
+					}
+
+					// +/- button
+					else if (index == 1) {
+					return MyButton(
+						buttonText: buttons[index],
+						color: Colors.blue[50],
+						textColor: Colors.black,
+					);
+					}
+					// % Button
+					else if (index == 2) {
+					return MyButton(
+						buttontapped: () {
+						setState(() {
+							userInput += buttons[index];
+						});
+						},
+						buttonText: buttons[index],
+						color: Colors.blue[50],
+						textColor: Colors.black,
+					);
+					}
+					// Delete Button
+					else if (index == 3) {
+					return MyButton(
+						buttontapped: () {
+						setState(() {
+							userInput =
+								userInput.substring(0, userInput.length - 1);
+						});
+						},
+						buttonText: buttons[index],
+						color: Colors.blue[50],
+						textColor: Colors.black,
+					);
+					}
+					// Equal_to Button
+					else if (index == 18) {
+					return MyButton(
+						buttontapped: () {
+						setState(() {
+							equalPressed();
+						});
+						},
+						buttonText: buttons[index],
+						color: Colors.orange[700],
+						textColor: Colors.white,
+					);
+					}
+
+					// other buttons
+					else {
+					return MyButton(
+						buttontapped: () {
+						setState(() {
+							userInput += buttons[index];
+						});
+						},
+						buttonText: buttons[index],
+						color: isOperator(buttons[index])
+							? Colors.blueAccent
+							: Colors.white,
+						textColor: isOperator(buttons[index])
+							? Colors.white
+							: Colors.black,
+					);
+					}
+				}), // GridView.builder
+			),
+		),
+		],
+	),
+	);
+}
+
+bool isOperator(String x) {
+	if (x == '/' || x == 'x' || x == '-' || x == '+' || x == '=') {
+	return true;
+	}
+	return false;
+}
+
+// function to calculate the input operation
+void equalPressed() {
+	String finaluserinput = userInput;
+	finaluserinput = userInput.replaceAll('x', '*');
+
+	Parser p = Parser();
+	Expression exp = p.parse(finaluserinput);
+	ContextModel cm = ContextModel();
+	double eval = exp.evaluate(EvaluationType.REAL, cm);
+	answer = eval.toString();
+}
 }
