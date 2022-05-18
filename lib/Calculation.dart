38896d7c6bc2aa4cae4e4fef5cc29ec4771392fa
arora <aroramadhav65@gmail.com>
2022-05-18 23:16:50 +0530

    Update

--- lib/Calculation.dart
+++ lib/Calculation.dart
@@ -155,22 +155,22 @@ class _CalculationState extends State<Calculation> {
       return;
     }
     setState(() {
-      switch (operator) {
-        case '+':
-          result = firstOperand! + secondOperand!;
-          break;
-        case '-':
-          result = firstOperand! - secondOperand!;
-          break;
-        case '*':
-          result = firstOperand! * secondOperand!;
-          break;
-        case '/':
-          if (secondOperand == 0) {
-            return;
-          }
-          result = firstOperand! ~/ secondOperand!;
-          break;
+      // switch (operator) {
+      //   case '+':
+      //     result = firstOperand! + secondOperand!;
+      //     break;
+      //   case '-':
+      //     result = firstOperand! - secondOperand!;
+      //     break;
+      //   case '*':
+      //     result = firstOperand! * secondOperand!;
+      //     break;
+      //   case '/':
+      //     if (secondOperand == 0) {
+      //       return;
+      //     }
+      //     result = firstOperand! ~/ secondOperand!;
+      //     break;
       }
 
       firstOperand = result;
