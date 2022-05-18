adee8520981987bbe889f20966ae39177c81a533
arora <aroramadhav65@gmail.com>
2022-05-18 17:40:47 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,6 @@ class _MyHomePageState extends State<MyHomePage> {
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
-      // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
