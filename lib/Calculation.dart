bd35b786f4b60b443bb2ab1686a5fb6b562f75cd
arora <aroramadhav65@gmail.com>
2022-05-18 20:35:41 +0530

    Update

--- lib/Calculation.dart
+++ lib/Calculation.dart
@@ -63,7 +63,7 @@ class _CalculationState extends State<Calculation> {
             _getButton(
                 text: '-',
                 onTap: () => operatorPressed('-'),
-                backgroundColor: Color.fromRGBO(220, 220, 220, 1)),
+                backgroundColor: Color.fromRGBO(220, 220, 220, 1),),
           ],
         ),
       ],
