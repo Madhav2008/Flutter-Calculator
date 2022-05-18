79263e6c60ec9d6d49334a24a162ee261b48bdde
arora <aroramadhav65@gmail.com>
2022-05-18 20:35:27 +0530

    Update

--- lib/Calculation.dart
+++ lib/Calculation.dart
@@ -16,16 +16,39 @@ class _CalculationState extends State<Calculation> {
       children: [
         ResultDisplay(text: '0'),
         Row(
-          children: [
-            CalculatorButton(
-              label: '7',
-              onTap: () => {},
-              size: 90,
-              backgroundColor: Colors.white,
-              labelColor: Colors.black,
-            )
-          ],
-        ),
+        children: [
+          _getButton(text: '7', onTap: () => numberPressed(7)),
+          _getButton(text: '8', onTap: () => numberPressed(8)),
+          _getButton(text: '9', onTap: () => numberPressed(9)),
+          _getButton(text: 'x', onTap: () => operatorPressed('*'), backgroundColor: Color.fromRGBO(220, 220, 220, 1)),
+        ],
+      ),
+      Row(
+        children: [
+          _getButton(text: '4', onTap: () => numberPressed(4)),
+          _getButton(text: '5', onTap: () => numberPressed(5)),
+          _getButton(text: '6', onTap: () => numberPressed(6)),
+          _getButton(text: '/', onTap: () => operatorPressed('/'), backgroundColor: Color.fromRGBO(220, 220, 220, 1)),
+        ],
+      ),
+      Row(
+        children: [
+          _getButton(text: '1', onTap: () => numberPressed(1)),
+          _getButton(text: '2', onTap: () => numberPressed(2)),
+          _getButton(text: '3', onTap: () => numberPressed(3)),
+          _getButton(text: '+', onTap: () => operatorPressed('+'), backgroundColor: Color.fromRGBO(220, 220, 220, 1))
+        ],
+      ),
+      Row(
+        children: [
+          _getButton(text: '=', onTap: calculateResult, backgroundColor: Colors.orange, textColor: Colors.white),
+          _getButton(text: '0', onTap: () => numberPressed(0)),
+          _getButton(text: 'C', onTap: clear, backgroundColor: Color.fromRGBO(220, 220, 220, 1)),
+          _getButton(text: '-', onTap: () => operatorPressed('-'),backgroundColor: Color.fromRGBO(220, 220, 220, 1)),
+        ],
+      ),
+    ]
+  );
       ],
     );
   }
