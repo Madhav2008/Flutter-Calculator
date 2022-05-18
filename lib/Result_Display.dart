4cbbaf1e54537ff7df68818f909070652df347c4
arora <aroramadhav65@gmail.com>
2022-05-18 20:10:20 +0530

    Update

--- lib/Result_Display.dart
+++ lib/Result_Display.dart
@@ -12,15 +12,16 @@ class ResultDisplay extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        width: double.infinity,
-        height: 80,
-        color: Colors.black,
-        child: Text(
-          text,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 34,
-          ),
-        ),);
+      width: double.infinity,
+      height: 80,
+      color: Colors.black,
+      child: Text(
+        text,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 34,
+        ),
+      ),
+    );
   }
 }
