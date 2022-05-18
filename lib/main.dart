6166325e0cbdd3f4f26e426edc37e59f0a24fcf4
arora <aroramadhav65@gmail.com>
2022-05-18 23:41:45 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -164,11 +164,13 @@ class _SimpleCalculatorState extends State<SimpleCalculator> {
                         buildButton("3", 1, Colors.black87),
                       ],
                     ),
-                    TableRow(children: [
-                      buildButton(".", 1, Colors.black87),
-                      buildButton("0", 1, Colors.black87),
-                      buildButton("00", 1, Colors.black87),
-                    ],),
+                    TableRow(
+                      children: [
+                        buildButton(".", 1, Colors.black87),
+                        buildButton("0", 1, Colors.black87),
+                        buildButton("00", 1, Colors.black87),
+                      ],
+                    ),
                   ],
                 ),
               ),
