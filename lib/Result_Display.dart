72c7bdece69b7e78d70728b2945deedece2e2723
arora <aroramadhav65@gmail.com>
2022-05-18 20:08:38 +0530

    Update

---
+++ lib/Result_Display.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ResultDisplay extends StatelessWidget {
+  ResultDisplay({
+    required this.text
+  });
+
+  final int result;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 80,
+      color: Colors.black,
+      child: Text(
+        text,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 34
+        ),
+      )
+    );
+  }
+}
