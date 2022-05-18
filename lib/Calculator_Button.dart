3306a584cb4e02baa92954995b332804c3f0f8d4
arora <aroramadhav65@gmail.com>
2022-05-18 20:33:30 +0530

    Update

--- lib/Calculator_Button.dart
+++ lib/Calculator_Button.dart
@@ -57,7 +57,7 @@ class CalculatorButton extends StatelessWidget {
 
 Widget _getButton({
   required String text,
-  fequired Function onTap,
+  required Function onTap,
   Color backgroundColor = Colors.white,
   Color textColor = Colors.black,
 }) {
