492517fad89b7919eee3cf987c8ffceb10388d6a
arora <aroramadhav65@gmail.com>
2022-05-18 17:41:07 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     // by the _incrementCounter method above.
-    //
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
