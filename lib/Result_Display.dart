c63a6d0091c7c4f255976b92a040158544d4bb08
arora <aroramadhav65@gmail.com>
2022-05-18 20:21:33 +0530

    Update

--- lib/Result_Display.dart
+++ lib/Result_Display.dart
@@ -12,20 +12,20 @@ class ResultDisplay extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
-      height: 80,
-      color: Colors.black,
-      child: Container(
-        alignment: Alignment.centerRight,
-        padding: EdgeInsets.only(right: 24),
-        child: Text(
-          text,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 34,
-          ),
-        ),
+    width: double.infinity,
+    height: 120,
+    color: Colors.black,
+    child: Container(
+      alignment: Alignment.bottomRight,
+      padding: EdgeInsets.only(right: 24, bottom: 24),
+      child: Text(
+        text,
+        style: TextStyle(
+        color: Colors.white,
+        fontSize: 34
       ),
-    );
+      )
+    )
+  );
   }
 }
