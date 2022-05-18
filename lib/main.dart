12e94bf55283df831381aa64feb47dcc2a237ae5
arora <aroramadhav65@gmail.com>
2022-05-18 20:04:51 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:  AppBar(
+      appBar: AppBar(
         title: new Text("Calculator"),
       ), //AppBar
       backgroundColor: Colors.white38,
