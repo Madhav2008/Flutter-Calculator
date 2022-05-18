b9ae34963e23a999628232217a26a6ea926f97a0
arora <aroramadhav65@gmail.com>
2022-05-18 20:09:48 +0530

    Update

--- lib/Result_Display.dart
+++ lib/Result_Display.dart
@@ -1,26 +1,23 @@
 import 'package:flutter/material.dart';
 
 class ResultDisplay extends StatelessWidget {
-
   ResultDisplay({
-    required this.text, required this.result,
+    required this.text,
+    required this.result,
   });
 
   final int result;
+  final String text;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
-      height: 80,
-      color: Colors.black,
-      child: Text(
-        text,
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 34
-        ),
-      )
-    );
+        width: double.infinity,
+        height: 80,
+        color: Colors.black,
+        child: Text(
+          text,
+          style: TextStyle(color: Colors.white, fontSize: 34),
+        ));
   }
 }
