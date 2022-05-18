ffa81c566141377a23122658f6239fb16adfa6ad
arora <aroramadhav65@gmail.com>
2022-05-18 20:27:55 +0530

    Update

--- lib/Calculator_Button.dart
+++ lib/Calculator_Button.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 
 class CalculatorButton extends StatelessWidget {
-  CalculatorButton({
-    required this.label,
-    required this.onTap,
-    required this.size,
-    this.backgroundColor = Colors.white,
-    this.labelColor = Colors.black
-  });
+  CalculatorButton(
+      {required this.label,
+      required this.onTap,
+      required this.size,
+      this.backgroundColor = Colors.white,
+      this.labelColor = Colors.black});
 
   final String label;
   final VoidCallback onTap;
@@ -18,34 +17,27 @@ class CalculatorButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-    padding: EdgeInsets.all(6),
-    child: Container(
-      width: size,
-      height: size,
-      decoration: BoxDecoration(
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey, 
-            offset: Offset(1, 1), 
-            blurRadius: 2
-          ),
-        ],
-        borderRadius: BorderRadius.all(
-          Radius.circular(size / 2)
+      padding: EdgeInsets.all(6),
+      child: Container(
+        width: size,
+        height: size,
+        decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey, offset: Offset(1, 1), blurRadius: 2),
+            ],
+            borderRadius: BorderRadius.all(Radius.circular(size / 2)),
+            color: backgroundColor),
+        child: FlatButton(
+          shape: CircleBorder(),
+          onPressed: onTap,
+          child: Center(
+              child: Text(
+            label,
+            style: TextStyle(fontSize: 24, color: labelColor),
+          )),
         ),
-        color: backgroundColor
       ),
-      child: FlatButton(
-  shape: CircleBorder(),
-  onPressed: onTap,
-  child: Center(
-    child: Text(
-      label,
-      style: TextStyle(fontSize: 24, color: labelColor),
-    )
-  ),
-),
-    ),
-  );
+    );
   }
 }
