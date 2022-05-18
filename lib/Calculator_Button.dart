74a5e7f5328bf6e50e6394e9b30b103e56c78fb3
arora <aroramadhav65@gmail.com>
2022-05-18 20:29:11 +0530

    Update

--- lib/Calculator_Button.dart
+++ lib/Calculator_Button.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 class CalculatorButton extends StatelessWidget {
-  CalculatorButton(
-      {required this.label,
-      required this.onTap,
-      required this.size,
-      this.backgroundColor = Colors.white,
-      this.labelColor = Colors.black,});
+  CalculatorButton({
+    required this.label,
+    required this.onTap,
+    required this.size,
+    this.backgroundColor = Colors.white,
+    this.labelColor = Colors.black,
+  });
 
   final String label;
   final VoidCallback onTap;
