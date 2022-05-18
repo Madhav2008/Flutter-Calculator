6555fdfd126c64ae3aed38232b5b198858c0365a
arora <aroramadhav65@gmail.com>
2022-05-18 20:21:41 +0530

    Update

--- lib/Result_Display.dart
+++ lib/Result_Display.dart
@@ -17,7 +17,7 @@ class ResultDisplay extends StatelessWidget {
         color: Colors.black,
         child: Container(
             alignment: Alignment.bottomRight,
-            padding: EdgeInsets.only(right: 24, bottom: 24),
+            padding: EdgeInsets.only(right: 24, bottom: 24,),
             child: Text(
               text,
               style: TextStyle(color: Colors.white, fontSize: 34),
