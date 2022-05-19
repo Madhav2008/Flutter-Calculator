44ac496eb19ab43928d8677c745481480a8bc744
arora <aroramadhav65@gmail.com>
2022-05-19 22:24:30 +0530

    Update

--- lib/Splash_Screen.dart
+++ lib/Splash_Screen.dart
@@ -34,10 +34,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Align(
-        alignment: Alignment.center,
-        
-      ),
+      body:
     );
   }
 }
