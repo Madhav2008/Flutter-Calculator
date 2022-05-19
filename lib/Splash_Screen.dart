400e11d3bb415122a767cee4bdc27caf60edec44
arora <aroramadhav65@gmail.com>
2022-05-19 22:24:13 +0530

    Update

--- lib/Splash_Screen.dart
+++ lib/Splash_Screen.dart
@@ -36,48 +36,7 @@ class _SplashScreenState extends State<SplashScreen> {
     return Scaffold(
       body: Align(
         alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              logo,
-              width: 85,
-              height: 85,
-            ),
-            SizedBox(
-              height: 250,
-            ),
-            Text(
-              'from',
-              style: TextStyle(
-                color: grey,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  '../assets/images/meta.png',
-                  color: secondary,
-                  width: 30,
-                  height: 30,
-                ),
-                Text(
-                  'Meta',
-                  style: TextStyle(
-                    color: secondary,
-                    letterSpacing: 3,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
+        
       ),
     );
   }
