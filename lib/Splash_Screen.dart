3f673c7c63a07c169cb63ad59339bda4f48b3dc9
arora <aroramadhav65@gmail.com>
2022-05-19 22:22:23 +0530

    Update

--- lib/Splash_Screen.dart
+++ lib/Splash_Screen.dart
@@ -2,7 +2,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:provider/provider.dart';
 import 'package:whatsapp/Constants/Constants.dart';
 import 'package:whatsapp/No_Connection/No_Connection.dart';
 
