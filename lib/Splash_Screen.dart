64a01b51019877d3a524f0746ed0dad69efe6ce3
arora <aroramadhav65@gmail.com>
2022-05-19 22:22:37 +0530

    Update

--- lib/Splash_Screen.dart
+++ lib/Splash_Screen.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: file_names, prefer_const_constructors, prefer_const_literals_to_create_immutables, prefer_const_constructors_in_immutables
 
 import 'package:flutter/material.dart';
-import 'package:whatsapp/Constants/Constants.dart';
 
 class SplashScreen extends StatefulWidget {
   SplashScreen({
