4a16c105c6a51e7b5586ef8661e6937625bddf83
arora <aroramadhav65@gmail.com>
2022-05-19 22:23:14 +0530

    Update

--- lib/Splash_Screen.dart
+++ lib/Splash_Screen.dart
@@ -33,9 +33,6 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final logo = Provider.of<ThemeProvider>(context).themeMode == ThemeMode.dark
-        ? '../assets/images/WhatsAppIndiaLogo1.png'
-        : '../assets/images/WhatsAppIndiaLogo.png';
     return FutureBuilder(
       future: _initialization,
       builder: (context, snapshot) {
