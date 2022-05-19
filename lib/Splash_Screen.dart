b8e9c9e1a9c7e40a87c3bceeafc9bef9fb1e8b5b
arora <aroramadhav65@gmail.com>
2022-05-19 22:42:47 +0530

    Update

--- lib/Splash_Screen.dart
+++ lib/Splash_Screen.dart
@@ -2,7 +2,6 @@
 
 import 'package:calculator/main.dart';
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
 
 class SplashScreen extends StatefulWidget {
   SplashScreen({
